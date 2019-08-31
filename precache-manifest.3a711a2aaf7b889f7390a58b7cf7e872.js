3e92ff338c3b83884d1b2d25997c2a71f82bf25a
Alexis Ortiz Ojeda <aortizoj15@gmail.com>
2019-08-31 01:19:31 -0700

    Updates

---
+++ precache-manifest.3a711a2aaf7b889f7390a58b7cf7e872.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c233daaa7bacaabe67663544a4da1883",
+    "url": "/recipenator/index.html"
+  },
+  {
+    "revision": "6744c59d7707ed580578",
+    "url": "/recipenator/static/css/main.21121eb7.chunk.css"
+  },
+  {
+    "revision": "049b09e44d50396e5b10",
+    "url": "/recipenator/static/js/2.f652d504.chunk.js"
+  },
+  {
+    "revision": "6744c59d7707ed580578",
+    "url": "/recipenator/static/js/main.f090dddc.chunk.js"
+  },
+  {
+    "revision": "3a8848c6f166aaad2b99",
+    "url": "/recipenator/static/js/runtime~main.b66ba8d4.js"
+  }
+]);
